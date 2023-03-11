d3632280d6c6c0ff93b61357af3d4883c6cad53b
Сергей <s3rg0sh4@gmail.com>
2023-03-11 14:10:29 +0300

    add file support and some refactor

---
+++ DipApi/Entities/FileDetails.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace DipApi.Entities
+{
+	public class FileDetails
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+		public string FileName { get; set; }
+		public byte[] FileData { get; set; }
+		public string FileType { get; set; }
+		public string UserId { get; set; }
+		public virtual User User { get; set; }
+
+	}
+}
--- DipApi/Entities/User.cs
+++ DipApi/Entities/User.cs
@@ -1,12 +1,23 @@
 namespace DipApi.Entities;
 
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
 using Microsoft.AspNetCore.Identity;
+
+
+#nullable disable
 
 public class User : IdentityUser
 {
 	public Guid NaturalPersonGuid { get; set; }
 	public string Token { get; set; } = string.Empty;
 	public DateTime? TokenExpires { get; set; }
+	//פאיכû
+	[ForeignKey("UserId")]
+	[JsonIgnore]
+	public virtual ICollection<FileDetails> FileDetails { get; set; }
+
 
 	public User(): base() { }
 	public User(string email) : base(email) 
